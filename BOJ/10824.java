6fe88faebd90e26481f0aa2bbc0049ae4705ef4a
양동혁 <lukigreen@naver.com>
2021-07-23 20:17:28 +0900

    백준 21.07.23

---
+++ BOJ/10824.java
@@ -0,0 +1,12 @@
+package justonly;
+
+import java.util.Scanner;
+
+public class Main_10824 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(Long.parseLong(sc.next() + sc.next()) + Long.parseLong(sc.next() + sc.next())); 
+	}
+
+}
