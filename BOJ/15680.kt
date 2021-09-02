1c5cb35d22600c63280eab8036b7fc55c2f1dd96
양동혁 <lukigreen@naver.com>
2021-09-03 02:11:18 +0900

    백준 21.09.03

---
+++ BOJ/15680.kt
@@ -0,0 +1,8 @@
+fun main() = with(System.`in`.bufferedReader()) {
+    if(readLine().toInt() == 0){
+        print("YONSEI")
+    } else {
+        print("Leading the Way to the Future")
+    }
+ }
+ 
