2d42cf19a6effbdc6367d00566a999a7025f48bc
양동혁 <lukigreen@naver.com>
2021-09-14 23:53:48 +0900

    백준 21.09.14

---
+++ BOJ/14918.kt
@@ -0,0 +1,8 @@
+package test
+
+import java.util.*
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val st = StringTokenizer(readLine())
+    print(st.nextToken().toInt() + st.nextToken().toInt())
+}
