a296d908bf289bfe1fc52cc38ac04969bfe5ddc0
SmiteFLame <lukigreen@naver.com>
2021-11-07 09:45:52 +0900

    백준 21.11.07

---
+++ BOJ/10102.kt
@@ -0,0 +1,22 @@
+package test
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+    val str = readLine()
+    var a = 0
+    var b = 0
+    str.forEach { now ->
+       if(now == 'A'){
+           a++
+       } else{
+           b++
+       }
+    }
+    if(a == b){
+        print("TIE")
+    } else if(a > b){
+        print("A")
+    } else{
+        print("B")
+    }
+}
