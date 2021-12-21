fe61d09cdf35926e7b2f62bb96187196d030f5eb
SmiteFLame <lukigreen@naver.com>
2021-12-21 10:51:43 +0900

    백준 21.12.21

---
+++ BOJ/1013.kt
@@ -0,0 +1,11 @@
+package test
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val sb = StringBuilder()
+    repeat(readLine().toInt()){
+        val str = readLine()
+        val ptn = "(100+1+|01)+"
+        sb.append(if (str.matches(Regex(ptn))) "YES\n" else "NO\n")
+    }
+    print(sb.toString())
+}
