3125ec580e5860aa2a56649ceb6250ff297b1dbe
SmiteFLame <lukigreen@naver.com>
2022-01-02 16:42:09 +0900

    백준 22.01.02

---
+++ BOJ/11023.kt
@@ -0,0 +1,12 @@
+package test
+
+import java.util.*
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val st = StringTokenizer(readLine())
+    var sum = 0
+    while(st.hasMoreTokens()){
+        sum += st.nextToken().toInt()
+    }
+    print(sum)
+}
