54ab0f79a7e61d8b0e894daf3c1afb04f54bc177
SmiteFLame <lukigreen@naver.com>
2022-01-24 08:01:01 +0900

    백준 22.01,.24

---
+++ BOJ/1773.kt
@@ -0,0 +1,23 @@
+package test
+
+import java.util.*
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val st = StringTokenizer(readLine())
+    val n = st.nextToken().toInt()
+    val c = st.nextToken().toInt()
+    val array = Array(c + 1) { false }
+    for (i in 0 until n) {
+        val x = readLine().toInt()
+        for (j in 1..(c / x)) {
+            array[j * x] = true
+        }
+    }
+    var cnt = 0
+    for (i in 1..c) {
+        if (array[i]) {
+            cnt++
+        }
+    }
+    print(cnt)
+}
