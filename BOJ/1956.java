a420d4b2ffda44c7befdf5678a0e0af3214e3d66
SmiteFLame <lukigreen@naver.com>
2022-02-13 16:26:16 +0900

    백준 22.02.13

--- BOJ/1956.java
+++ BOJ/1956.java
@@ -1,44 +1,74 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class Main_1956 {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	
-	static final int MAX = 4000000;
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static final int INF = 987654321;
 
-	public static void main(String[] args) throws IOException {
-		st = new StringTokenizer(br.readLine());
-		int V = Integer.parseInt(st.nextToken());
-		int E = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        st = new StringTokenizer(br.readLine());
 
-		int[][] array = new int[V + 1][V + 1];
-		for (int i = 1; i <= V; i++) {
-			Arrays.fill(array[i], MAX);
-		}
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
 
-		for (int i = 0; i < E; i++) {
-			st = new StringTokenizer(br.readLine());
-			array[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())] = Integer
-					.parseInt(st.nextToken());
-		}
+        int[][] arr = new int[V + 1][V + 1];
 
-		for (int k = 1; k <= V; k++) {
-			for (int i = 1; i <= V; i++) {
-				for (int j = 1; j <= V; j++) {
-					array[i][j] = Math.min(array[i][j], array[i][k] + array[k][j]);
-				}
-			}
-		}
+        for (int i = 1; i <= V; i++) {
+            for (int j = 1; j <= V; j++) {
+                if (i != j) {
+                    arr[i][j] = INF;
+                }
+            }
+        }
 
-		int answer = MAX;
-		for (int i = 1; i <= V; i++) {
-			answer = Math.min(answer, array[i][i]);
-		}
-		System.out.println(answer == MAX ? -1 : answer);
-	}
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            arr[a][b] = c;
+        }
+
+        for (int k = 1; k <= V; k++) {
+            for (int i = 1; i <= V; i++) {
+                for (int j = 1; j <= V; j++) {
+                    if (i == j) {
+                        continue;
+                    }
+
+                    if (arr[i][j] > arr[i][k] + arr[k][j]) {
+                        arr[i][j] = arr[i][k] + arr[k][j];
+                    }
+                }
+            }
+        }
+
+        int ans = INF;
+        for (int i = 1; i <= V; i++) {
+            for (int j = 1; j <= V; j++) {
+                if (i == j) {
+                    continue;
+                }
+
+                if (arr[i][j] != INF && arr[j][i] != INF) {
+                    ans = Math.min(ans, arr[i][j] + arr[j][i]);
+                }
+            }
+        }
+
+        ans = (ans == INF) ? -1 : ans;
+
+        bw.write(ans + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 
 }
