731eeeffaa89322b92ee3ed45162cfa0c93c1336
SmiteFLame <lukigreen@naver.com>
2022-04-30 17:27:54 +0900

    백준 22.04.30

---
+++ BOJ/20499.scala
@@ -0,0 +1,15 @@
+import scala.io.StdIn
+
+object Main {
+  def main(args: Array[String]) = {
+    val input_nums_string = StdIn.readLine().split("/")
+    val K = input_nums_string(0).toInt
+    val D = input_nums_string(1).toInt
+    val A = input_nums_string(2).toInt
+    if(K + A > D){
+      print("gosu")
+    } else{
+      print("hasu")
+    }
+  }
+}
