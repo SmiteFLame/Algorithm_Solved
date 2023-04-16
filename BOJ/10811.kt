b7c4ed4b84e10bd93d9e54ae54a2d9f7ea39c762
SmiteFLame <lukigreen@naver.com>
2023-04-16 17:20:31 +0900

    백준 23.04.16 SWAP

--- BOJ/10811.kt
+++ BOJ/10811.kt
@@ -7,20 +7,24 @@ fun main() = with(System.`in`.bufferedReader()) {
     var st = StringTokenizer(readLine())
     val n = st.nextToken().toInt()
     val m = st.nextToken().toInt()
-    val array = Array(n + 1) {0}
-    for(i in 1..n){
+    val array = Array(n + 1) { 0 }
+    for (i in 1..n) {
         array[i] = i
     }
 
-    repeat(m){
+    repeat(m) {
         st = StringTokenizer(readLine())
         val left = st.nextToken().toInt()
-        val right = st.nextToken().toInt()
-        val temp = array[left]
-        array[left] = array[right]
-        array[right] = temp
+        val right = st.nextToken().toInt() + 1
+        val temp = Array(right - left + 1) { 0 }
+        for(i in 0 until (right - left)){
+            temp[i] = array[left + i]
+        }
+        for(i in 0 until (right - left)){
+            array[right - i - 1] = temp[i]
+        }
     }
-    for(i in 1..n){
+    for (i in 1..n) {
         print("${array[i]} ")
     }
 }
